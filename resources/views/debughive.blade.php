051cb9bd2d6413633d542a44a6e735879490f459
Damian <ocyrus.dh@gmail.com>
2017-10-20 00:13:22 -0400

    Generate a non-image debughive logo from only HTML and CSS

--- resources/views/debughive.blade.php
+++ resources/views/debughive.blade.php
@@ -7,11 +7,23 @@
         <link rel="stylesheet" href="css/app.css">
 
     </head>
-    <body>
+    <body class="debughive">
         <div class="logo no_select">
             <div class="hexagon one">1</div>
             <div class="hexagon two">2</div>
             <div class="hexagon three">3</div>
+            <div class="debug">
+                <div class="head">
+                    <div class="antena antena_l">
+                        <div></div>
+                    </div>
+                    <div class="antena antena_r">
+                        <div></div>
+                    </div>
+                </div>
+                <div class="wing_l"></div>
+                <div class="wing_r"></div>
+            </div>
         </div>
     </body>
 </html>
