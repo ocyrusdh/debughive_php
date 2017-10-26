1b1c9a1c132b8f25ea67edee9bedf6aceaea6cf6
Damian <ocyrus.dh@gmail.com>
2017-10-25 20:34:58 -0400

    Add new font and styling to debughive main page

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,7 +5,7 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+//require('./bootstrap');
 
 window.Vue = require('vue');
 
@@ -15,8 +15,23 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+//Vue.component('example', require('./components/Example.vue'));
+
+
+
 
 const app = new Vue({
-    el: '#app'
+	el: '#debughive',
+	data: { nav: [	{ value: 'About' },
+						{ value: 'Main' },
+						{ value: 'Dev' }],
+			active_nav: 1
+			},
+	methods: {
+		isActive(index) {
+			this.active_nav = index;
+		}
+	},
+	mounted() {
+	}
 });
--- resources/views/debughive.blade.php
+++ resources/views/debughive.blade.php
@@ -10,9 +10,9 @@
     <body>
         <div id="debughive">
             <div id="logo">
-                <div :class="['hexagon', 'one', {'active' : hexagons[0].active}]" @click="isActive(0)" v-text="hexagons[0].value"></div>
-                <div :class="['hexagon', 'two', {'active' : hexagons[1].active}]" @click="isActive(1)" v-text="hexagons[1].value"></div>
-                <div :class="['hexagon', 'three', {'active' : hexagons[2].active}]" @click="isActive(2)" v-text="hexagons[2].value"></div>
+                <div :class="['hexagon', 'one', {'active' : active_nav == 0}]" @click="isActive(0)" v-text="nav[0].value"></div>
+                <div :class="['hexagon', 'two', {'active' : active_nav == 1}]" @click="isActive(1)" v-text="nav[1].value"></div>
+                <div :class="['hexagon', 'three', {'active' : active_nav == 2}]" @click="isActive(2)" v-text="nav[2].value"></div>
                 <div class="debug">
                     <div class="head">
                         <div class="antena antena_l">
@@ -25,9 +25,11 @@
                     <div class="wing_l"></div>
                     <div class="wing_r"></div>
                 </div>
+                <div id="logo_text">ebug</div>
             </div>
             <div id="hive">
-                <div>
+                <div class="connection_line"></div>
+                <div class="container">
                     @for($i = 0; $i < 50; $i++)
                         <div>
                             <div class="hexagon"></div>
